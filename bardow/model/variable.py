399a215ac949ab0ecf46e371308bf909402d5075
LuNoX <maillunox@gmail.com>
2024-01-18 02:52:45 +0100

    Integrated SymPy

--- bardow/model/variable.py
+++ bardow/model/variable.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
 from abc import ABC, abstractmethod
+from typing import Optional
 
+import sympy
 from sympy.physics import units
+from sympy.physics.units import systems
 
 
 @dataclass
@@ -16,19 +19,38 @@ class Variable(ABC):
 
 @dataclass
 class Known(Variable):
-    var_unit: units.Unit
+    unit: units.Unit
     value: float
-    # TODO: proper formatting
-    representation_format: str = "{value} {var_unit}"
+    symbol: Optional[str] = None
+
+    __sympy_quantity: units.Quantity = None
+
+    @property
+    def _sympy_quantity(self) -> units.Quantity:
+        if self.__sympy_quantity is not None:
+            return self.__sympy_quantity
+        quantity = units.Quantity(self.name)
+        systems.SI.set_quantity_dimension(quantity, self.unit.dimension)
+        systems.SI.set_quantity_scale_factor(quantity, self.value * self.unit)
+        self.__sympy_quantity = quantity
+        return quantity
 
     def formula_representation(self) -> str:
-        return self.representation_format.format(value=self.value,
-                                                 var_unit=self.var_unit)
+        return str(self.__sympy_quantity.convert_to(systems.SI._base_units))
 
 
 @dataclass
 class Unknown(Variable):
     symbol: str
+    __sympy_symbol: sympy.Symbol = None
+
+    @property
+    def _sympy_symbol(self) -> sympy.Symbol:
+        if self.__sympy_symbol is not None:
+            return self.__sympy_symbol
+        symbol = sympy.Symbol(self.symbol)
+        self.__sympy_symbol = symbol
+        return symbol
 
     def formula_representation(self) -> str:
         return self.symbol
