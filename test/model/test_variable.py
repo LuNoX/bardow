b388b57f445d91b6449f696a76a28324a8e317a1
LuNoX <maillunox@gmail.com>
2024-08-16 11:08:26 +0200

    Added tests for Variable class

--- test/model/test_variable.py
+++ test/model/test_variable.py
@@ -1,9 +1,33 @@
 import unittest
+from unittest.mock import patch
+
+from bardow.model import variable
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestVariable(unittest.TestCase):
+
+    def test_instantiation(self) -> None:
+        with self.assertRaises(TypeError):
+            variable.Variable(name="test")
+
+    @patch.multiple(variable.Variable, __abstractmethods__=set())
+    def test_fields(self) -> None:
+        name = "test"
+        self.instance = variable.Variable(name=name)
+        self.assertEqual(self.instance.name, name)
+        self.assertIs(self.instance.dimension, None)
+
+
+class TestKnown(unittest.TestCase):
+    # TODO: implement
+    def test_instantiation(self) -> None:
+        ...
+
+
+class TestUnknown(unittest.TestCase):
+    # TODO: implement
+    def test_instantiation(self) -> None:
+        ...
 
 
 if __name__ == '__main__':
