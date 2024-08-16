717c81ce1d456b4ed00526f5bb4d2cd65d62adee
LuNoX <maillunox@gmail.com>
2024-08-16 10:36:38 +0200

    Made Table an abstract class

--- bardow/model/table.py
+++ bardow/model/table.py
@@ -1,12 +1,13 @@
 import pandas as pd
 from typing import Self, Set
 from dataclasses import dataclass
+from abc import ABC, abstractmethod
 
 from bardow.model import variable
 
 
 @dataclass
-class Table:
+class Table(ABC):
     variables: Set[variable.Variable]
     data: pd.DataFrame
 
@@ -31,10 +32,19 @@ class Table:
         pass
 
     def lookup(self, input_variables: Set[variable.Known],
-               only_return: Set[variable.Variable] = None):
+               only_return: Set[variable.Variable] = None
+               ) -> Set[variable.Known]:
         if only_return is None:
             only_return = self.variables
-        return
+        return self._lookup(input_variables, only_return)
 
-    def interpolate(self):
-        pass
+    @abstractmethod
+    def _lookup(self, input_variables: Set[variable.Known],
+                target_variables: Set[variable.Variable]
+                ) -> Set[variable.Known]:
+        raise NotImplementedError()
+
+    @staticmethod
+    def interpolate():
+        # TODO: implement
+        ...
