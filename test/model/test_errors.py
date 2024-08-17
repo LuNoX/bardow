84c28f0c61bf5430754c7e142fc9d1967f4fc448
LuNoX <maillunox@gmail.com>
2024-08-18 01:10:59 +0200

    Added tests

--- test/model/test_errors.py
+++ test/model/test_errors.py
@@ -1,10 +1,24 @@
-import unittest
+import pytest
+
+from bardow.model import errors
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TestErrors:
+    @staticmethod
+    def test_cannot_convert_error() -> None:
+        with pytest.raises(errors.CannotConvertError):
+            raise errors.CannotConvertError("This is a test error")
+
+    @staticmethod
+    def test_not_solvable_error() -> None:
+        with pytest.raises(errors.NotSolvableError):
+            raise errors.NotSolvableError("This is a test error")
+
+    @staticmethod
+    def test_too_few_knowns_error() -> None:
+        with pytest.raises(errors.TooFewKnownsError):
+            raise errors.TooFewKnownsError("This is a test error")
 
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()
